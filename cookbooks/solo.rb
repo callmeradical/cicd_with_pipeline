8fe35e93badbf0a08ca0825cced667a7ab33146d
Lars Cromley <lars@paniksrvr.info>
2016-05-05 03:50:15 -0400

    setting up codedeploy

---
+++ cookbooks/solo.rb
@@ -0,0 +1,7 @@
+file_cache_path "c:/etc/chef"
+cookbook_path [
+      "c:/chef/cookbooks",
+      "c:/chef/site-cookbooks"
+              
+]
+json_attribs "c:/chef/cookbooks/node.json"
---
+++ scripts/chef-solo.bat
@@ -0,0 +1,1 @@
+c:/opscode/chef/bin/chef-solo -c c:/chef/cookbooks/solo.rb
